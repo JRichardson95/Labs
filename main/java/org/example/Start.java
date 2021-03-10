afdb98381102bc3d8c5673c241ab4685f13520c1
JRichardson95 <71064586+JRichardson95@users.noreply.github.com>
2021-03-10 14:25:18 +0100

    FirstTaste aangemaakt

---
+++ main/java/org/example/Start.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+            FirstTaste ft1 = new FirstTaste();
+            ft1.aantalReizigers = 30;
+            ft1.doeIets();
+    }
+
+}
