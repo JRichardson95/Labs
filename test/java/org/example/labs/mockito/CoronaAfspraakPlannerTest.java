3fd8aeefa523d224e8d14aeed1ac0c2340e071f3
JRichardson95 <71064586+JRichardson95@users.noreply.github.com>
2021-03-23 15:16:34 +0100

    Mockito testen verbeterd

--- test/java/org/example/labs/mockito/CoronaAfspraakPlannerTest.java
+++ test/java/org/example/labs/mockito/CoronaAfspraakPlannerTest.java
@@ -1,26 +1,37 @@
 package org.example.labs.mockito;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import static org.mockito.Mockito.*;
 
 
 class CoronaAfspraakPlannerTest {
-    CoronaAfspraakPlanner target;
-    SystemInWrapper in = mock(SystemInWrapper.class);
-    SoutWrapper out = mock(SoutWrapper.class);
+    private static final String BSN = "1234";
+    private static final String DATE = "2021";
+    private ArgumentCaptor<String> arg;
+    private CoronaAfspraakPlanner target;
+    private SystemInWrapper in = mock(SystemInWrapper.class);
+    private SoutWrapper out = mock(SoutWrapper.class);
 
     @BeforeEach
     void setUp() {
+        arg = ArgumentCaptor.forClass(String.class);
+
         target = new CoronaAfspraakPlanner();
         target.setIn(in);
         target.setOut(out);
-        when(in.nextLine()).thenReturn("1234");
+
+        when(in.nextLine()).thenReturn(BSN);
+
+        doNothing().when(out).print(arg.capture());
+        doNothing().when(out).printLn(arg.capture());
     }
 
     @Test
-    void printFirstLine(){
+    void printFirstLine() {
         target.run();
         verify(out, times(1)).print(anyString());
     }
@@ -32,15 +43,17 @@ class CoronaAfspraakPlannerTest {
     }
 
     @Test
-    void printBSN(){
+    void printBSN() {
         target.run();
-        verify(out, times(1)).printLn(contains("1234"));
+        verify(out, times(1)).printLn(contains(BSN));
+        Assert.assertTrue(arg.getAllValues().get(1).contains(BSN));
+        Assert.assertFalse(arg.getAllValues().get(1).contains(BSN + 1));
     }
 
     @Test
-    void printDate(){
+    void printDate() {
         target.run();
-        verify(out).printLn(contains("2021"));
+        Assert.assertTrue(arg.getAllValues().get(2).contains(DATE));
     }
 
 }
