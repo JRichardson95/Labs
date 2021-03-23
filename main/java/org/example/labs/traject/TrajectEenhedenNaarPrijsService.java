5738a9434f1ebea27c8c2bd0e51d8b12607ae29c
JRichardson95 <71064586+JRichardson95@users.noreply.github.com>
2021-03-23 14:09:15 +0100

    Deleted Traject

--- main/java/org/example/labs/traject/TrajectEenhedenNaarPrijsService.java
+++
@@ -1,4 +0,0 @@
-package org.example.labs.traject;
-
-public class TrajectEenhedenNaarPrijsService {
-}
--- main/java/org/example/labs/traject/TrajectPrijsService.java
+++
@@ -1,8 +0,0 @@
-package org.example.labs.traject;
-
-public class TrajectPrijsService {
-
-    TrajectNaarTrajectEenhedenService
-
-
-}
